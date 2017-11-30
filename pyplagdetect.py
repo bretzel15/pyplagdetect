c5b9a895587576398f324455d1195061c21e8e57
bretzel15 <bretzel15@users.noreply.github.com>
2017-11-30 11:34:55 +0100

    Add copyright notice in beginning of file

--- pyplagdetect.py
+++ pyplagdetect.py
@@ -1,5 +1,23 @@
 #! /usr/bin/python3
 
+#   Copyright 2017 - Tobias Esswein
+#
+#   This file is part of pyplagdetect.
+#
+#   pyplagdetect is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   any later version.
+#
+#   pyplagdetect is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # edit directories for existing and new reports, relative to script folder
 existing_dir = "../existing_reports"
 test_dir = "../to_be_tested"
